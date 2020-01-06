80f3ffd63c9165e5607cf0659cf18389542d7880
Chris Grimmett <chris@grimtech.net>
2020-01-06 01:37:05 -0800

    optimize for neocities deploy

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import NameThatVocaloid from '@/components/NameThatVocaloid';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   routes: [{
       path: '/',
       component: Home
